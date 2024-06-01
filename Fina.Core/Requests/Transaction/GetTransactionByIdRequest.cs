95b98c5614f96bbd8f71a6eb83daed791c9c0eb1
gustavo <gustavo.singeski@gmail.com>
2024-06-01 02:12:33 -0300

    Adicionar arquivos de projeto.

---
+++ Fina.Core/Requests/Transaction/GetTransactionByIdRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Requests.Transaction
+{
+    public class GetTransactionByIdRequest : Request
+    {
+        [Required(ErrorMessage = "Id inválido")]
+        public long Id { get; set; }
+    }
+}
